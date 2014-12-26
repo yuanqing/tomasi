85195dc73dc952eb7c1ca4209d813e83951f108f
yuanqing <hello@yuanqing.sg>
2014-12-26 13:00:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# tomasi.js [![Experimental](http://img.shields.io/badge/stability-experimental-red.svg?style=flat)](https://github.com/yuanqing/tomasi) [![npm Version](http://img.shields.io/npm/v/tomasi.svg?style=flat)](https://www.npmjs.org/package/tomasi) [![Build Status](https://img.shields.io/travis/yuanqing/tomasi.svg?style=flat)](https://travis-ci.org/yuanqing/tomasi) [![Coverage Status](https://img.shields.io/coveralls/yuanqing/tomasi.svg?style=flat)](https://coveralls.io/r/yuanqing/tomasi)
+
+> An extensible build system for making static websites.
+
+## License
+
+[MIT license](https://github.com/yuanqing/tomasi/blob/master/LICENSE)
---
+++ index.js
@@ -0,0 +1,122 @@
+'use strict';
+
+var _ = require('savoy');
+var clone = require('clone');
+var defaults = require('defaults');
+var extend = require('extend');
+var fastmatter = require('fastmatter');
+var fs = require('fs');
+var glob = require('glob');
+var isUtf8 = require('is-utf8');
+var noop = function() {};
+var pppath = require('pppath');
+
+var tomasi = function(config, cb) {
+
+  config = defaults(config, {
+    linkPrefix: '/',
+    inDir: './in',
+    outDir: './out',
+    outFile: 'index.html',
+    tmplDir: './tmpl',
+    tmplEngine: 'ejs',
+    dataTypes: {}
+  });
+
+  _.waterfall({
+    read: function(cb) {
+      read(cb, config);
+    },
+    pipe: function(cb, dataTypes) {
+      var cbWrap = function(err, i) {
+        if (err || i === true) {
+          return cb(err);
+        }
+        pipe(cbWrap, config, dataTypes, i);
+      };
+      cbWrap(null, 0);
+    }
+  }, function(err) {
+    (cb || noop)(err);
+  });
+
+};
+
+var read = function(cb, config) {
+
+  _.map(config.dataTypes, function(cb, configDataType) {
+    var pattern = pppath([config.inDir, configDataType.in]);
+    readFiles(cb, pattern, configDataType.out);
+  }, function(err, dataTypes) {
+    cb(err, dataTypes);
+  });
+
+};
+
+var readFiles = function(cb, pattern, viewsConfig) {
+
+  _.waterfall({
+    glob: function(cb) {
+      glob(pattern, function(err, filenames) {
+        cb(err, filenames);
+      });
+    },
+    read: function(cb, filenames) {
+      _.map(filenames, function(cb, filename) {
+        readFile(cb, pattern, filename);
+      }, function(err, files) {
+        cb(err, files);
+      });
+    },
+    clone: function(cb, files) {
+      cb(null, _.map(viewsConfig, function(viewConfig) {
+        return { config: viewConfig, files: clone(files) };
+      }));
+    }
+  }, function(err, dataTypes) {
+    cb(err, dataTypes);
+  });
+
+};
+
+var readFile = function(cb, pattern, filename) {
+
+  fs.readFile(filename, function(err, buffer) {
+    if (err) { return cb(err); }
+    var file = {};
+    if (isUtf8(buffer)) {
+      var fields = fastmatter(buffer.toString());
+      extend(file, fields.attributes, { $content: fields.body });
+    } else {
+      file.$content = buffer;
+    }
+    cb(null, file);
+  });
+
+};
+
+var pipe = function(cb, config, dataTypes, i) {
+
+  var done = true;
+  _.each(dataTypes, function(cb, views) {
+    _.each(views, function(cb, view) {
+      var pipes = [].concat(view.config[i]).filter(Boolean);
+      if (!pipes.length) {
+        return cb();
+      }
+      done = false;
+      _.each(pipes, function(cb, pipe) {
+        pipe(cb, view.files.filter(Boolean), dataTypes, config);
+      }, function(err) {
+        cb(err);
+      });
+    }, function(err) {
+      cb(err);
+    });
+  }, function(err) {
+    cb(err, done || i + 1);
+  });
+
+};
+
+module.exports = tomasi;
