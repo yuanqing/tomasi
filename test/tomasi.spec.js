7483cde56d54f34e68ead39b92a36c888caab78a
yuanqing <hello@yuanqing.sg>
2014-12-26 14:23:41 +0800

    Add boilerplate for tests

---
+++ test/tomasi.spec.js
@@ -0,0 +1,12 @@
+/* globals describe, it, expect */
+'use strict';
+
+var tomasi = require('..');
+
+describe('tomasi([config, cb])', function() {
+
+  it('', function() {
+    expect(tomasi).not.toBeUndefined();
+  });
+
+});
