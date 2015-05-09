8528cc738def5163a15a672ceb6a11a087207c7e
yuanqing <hello@yuanqing.sg>
2015-05-09 16:28:45 +0800

    Fix `in` file resolution

--- test/fixtures/tomasi.js
+++ test/fixtures/tomasi.js
@@ -1,13 +1,10 @@
 'use strict';
 
-var path = require('path');
-var inPath = path.join(__dirname, '*.txt');
-
 module.exports = function() {
 
   return {
     blog: {
-      in: inPath,
+      in: '*.txt',
       out: {
         single: []
       }
