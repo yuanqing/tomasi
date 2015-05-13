b3f7a98447d2e00cb5d0efaf8e42badf135ae985
yuanqing <hello@yuanqing.sg>
2015-05-14 02:48:07 +0800

    Refactor tests for `tomasi.serve`

--- test/serve.js
+++ test/serve.js
@@ -15,22 +15,34 @@ test('throws if `$outDir` not found', function(t) {
   t.end();
 });
 
-test('serves the `$outDir` on port 8888', function(t) {
-  var outDir = 'out';
-  t.false(fs.existsSync(outDir));
-  var file = join(outDir, 'foo.txt');
+var setUp = function(t) {
+  t.false(fs.existsSync('out'));
+  var file = join('out', 'foo.txt');
   fs.outputFileSync(file, 'foo');
   t.true(fs.existsSync(file));
+};
+
+var tearDown = function(t) {
+  fs.removeSync('out');
+  t.false(fs.existsSync('out'));
+};
+
+test('serves the `$outDir` on port 8888', function(t) {
+  setUp(t);
   var server = tomasi({}).serve();
   request('http://localhost:8888/foo.txt', function(err, res, body) {
-    fs.removeSync(outDir);
-    t.false(fs.existsSync(outDir));
+    tearDown(t);
     server.close();
     if (err || res.statusCode !== 200) {
       t.fail();
     } else {
       t.equal(body, 'foo');
     }
+    t.end();
+  }).on('error', function() {
+    tearDown(t);
+    server.close();
+    t.fail();
     t.end();
   });
 });
